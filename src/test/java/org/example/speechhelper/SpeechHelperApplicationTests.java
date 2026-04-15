a364afd0bca3db11b081b96b2f10f9cea0ac6382
boxinthechaos <kdmwp310@gmail.com>
2026-04-15 12:00:42 +0900

    SpeechHelper

---
+++ src/test/java/org/example/speechhelper/SpeechHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.speechhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
